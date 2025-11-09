fd51211340049e3ad1693bd625e1956114b97f11
Ngoc Giau <anngocgiang2209@gmail.com>
2025-11-09 22:06:34 +0700

    Bao cao tuan 4

---
+++ project/ThucTapChuyenNganhh/ThucTapChuyenNganhh/Controllers/AboutController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ThucTapChuyenNganhh.Models;
+
+namespace ThucTapChuyenNganhh.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ project/ThucTapChuyenNganhh/ThucTapChuyenNganhh/Controllers/AdminController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ThucTapChuyenNganhh.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Charts()
+        {
+            return View();
+        }
+        public IActionResult Tables()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Navbar()
+        {
+            return View();
+        }
+        public IActionResult Cards()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Forgot_Pw()
+        {
+            return View();
+        }
+        public IActionResult Blank()
+        {
+            return View();
+        }
+    }
+}
